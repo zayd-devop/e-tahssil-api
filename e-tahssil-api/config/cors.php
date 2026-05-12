4bd3c3cb9e75336e74a9fefa83afd212f8173cfe
zaid <saousaouzaid10@gmail.com>
2026-05-12 18:47:06 +0100

    error fix

--- e-tahssil-api/config/cors.php
+++ e-tahssil-api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173','https://e-tahssil.vercel.app'],
+    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173','https://e-tahssil.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
